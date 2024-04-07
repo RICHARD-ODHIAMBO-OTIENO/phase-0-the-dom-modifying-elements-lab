27a550c69e042b10079441a86802873843373e2d
Richard Odhiambo Otieno <kotienor1@gmail.com>
2024-04-07 20:27:31 +0300

    phase-0-dom-modifying-element-lab

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Write your code here!
+document.getElementById('main').remove();
+
+// create newHeader variable that points to an <h1> node
+let newHeader = document.createElement('h1');
+
+// newHeader variable that points to the <h1> node has an id of 'victory'
+newHeader.id = 'victory';
+
+// Set the text inside the 'newHeader' variable to "YOUR-NAME 
+newHeader.textContent = 'Richard is the champion';
